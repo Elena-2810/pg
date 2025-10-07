bd777b6d72ec9c9f5a53a780304a933a095e609f
Elena <lenatolmaceva20@gmail.com>
2025-10-07 14:43:34 +0000

    Cvičení 1

---
+++ first.py
@@ -0,0 +1,14 @@
+def sudy_nebo_lichy(cislo):
+ 
+    if cislo % 2 == 0:
+    
+        print(f"Číslo {cislo} je sudé.")
+    
+    else:
+        print(f"Číslo {cislo} je liché.")
+
+
+print("Spouštím testování funkce...")
+sudy_nebo_lichy(5)          
+sudy_nebo_lichy(1000000)    
+print("...testování dokončeno.")
