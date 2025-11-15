57aea1a4e594ff8e12154a8b8d600b4aa6eb5ada
Olena Tolmachova <lenatolmaceva20@gmail.com>
2025-11-15 14:09:45 +0100

    Implement file type checking function
    Added a new function to check file types and print result.

--- fifth.py
+++ fifth.py
@@ -1,6 +1,5 @@
 import sys
 
-
 jpeg_header = b'\xff\xd8'
 gif_header1 = b'GIF87a'
 gif_header2 = b'GIF89a'
@@ -8,7 +7,6 @@ png_header = b'\x89PNG\r\n\x1a\n'
 
 
 def read_header(file_name, header_length):
-    
     try:
         with open(file_name, "rb") as f:
             header = f.read(header_length)
@@ -19,30 +17,37 @@ def read_header(file_name, header_length):
 
 
 def is_jpeg(file_name):
-    
     header = read_header(file_name, len(jpeg_header))
     return header == jpeg_header
 
 
 def is_gif(file_name):
-   
     header = read_header(file_name, len(gif_header1))
     return header == gif_header1 or header == gif_header2
 
 
 def is_png(file_name):
-   
     header = read_header(file_name, len(png_header))
     return header == png_header
 
 
+def print_file_type(file_name):
+    
+    if is_jpeg(file_name):
+        return True
+    elif is_gif(file_name):
+        return True
+    elif is_png(file_name):
+        return True
+    else:
+        return False
+
+
 if __name__ == "__main__":
     try:
-        file_name = sys.argv[1]  
-      
-        if is_jpeg(file_name) or is_gif(file_name) or is_png(file_name):
-            print(True)
-        else:
-            print(False)
+        file_name = sys.argv[1]
+        result = print_file_type(file_name)   
+        print(result)                         
     except Exception as e:
         print(f"Chyba: {e}")
+
